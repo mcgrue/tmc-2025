f50a1e1760d91d94fac125211dc975b55e56c040
Ben McGraw <ben.mcgraw@gmail.com>
2025-01-07 13:12:26 -0800

    format upon save now

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,22 +1,13 @@
-
-
-
 export default function Home() {
   return (
-
-        
-        
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            I hate tailwind
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            ...
-          </li>
-          
-        </ol>
-
-
+    <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+      <li className="mb-2">
+        I hate tailwind
+        <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
+          src/app/page.tsx
+        </code>
+        ...
+      </li>
+    </ol>
   );
 }
